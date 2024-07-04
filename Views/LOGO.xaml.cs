7fd03d4c074c9effb984dc4eecc03037d8b2ae53
saurabh-band <saurabh1993.sb@gmail.com>
2024-07-05 02:16:54 +0530

    LogoPage

--- Views/LOGO.xaml.cs
+++ Views/LOGO.xaml.cs
@@ -1,9 +1,20 @@
+using Microsoft.Maui.Controls;
+
 namespace CascadeProtoypeA.Views;
 
 public partial class LOGO : ContentPage
 {
-	public LOGO()
+    string clickTotal;
+
+    public LOGO()
 	{
 		InitializeComponent();
-	}
+        BindingContext=this;
+    }
+    void OnImageButtonClicked(object sender, EventArgs e)
+    {
+        int click = 0;
+        click += 1;
+        clickTotal = click.ToString();
+    }
 }
